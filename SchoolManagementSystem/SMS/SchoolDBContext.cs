bb4ee771b98a2876dafca2ed080381fbc56a7795
rshanthos89 <55778564+rshanthos89@users.noreply.github.com>
2019-12-08 20:05:17 +0530

    Database creation & Migration done

---
+++ SchoolManagementSystem/SMS/SchoolDBContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMS.DataSource
+{
+    public class SchoolDBContext : DbContext
+    {
+        //public SchoolDBContext(DbContextOptions<SchoolDBContext> options) : base(options)
+        //{
+            
+        //    Database.Migrate();
+        //}
+
+        // add classes in here to include to database
+        public DbSet<Admin> Admins { get; set; }
+
+        public DbSet<SclPrincipal> Principals { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<ClassRoom> ClassRooms { get; set; }
+
+        //Making the connection
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) //to create databse
+        {
+            string sqlConnection = "Server=SANTHOSH;Database=SchoolManagementSystemDB;Trusted_Connection=true"; //Don't leave any space in the string
+            builder.UseSqlServer(sqlConnection);
+
+            base.OnConfiguring(builder);
+        }
+    }
+}
